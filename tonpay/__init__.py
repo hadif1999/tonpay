1fad8bf0244a395447e6b091334823e6d56d593a
hadif1999 <hadifathi1378138@gmail.com>
2024-08-01 19:06:41 +0330

    logging with loguru added

--- tonpay/__init__.py
+++ tonpay/__init__.py
@@ -1,7 +1,38 @@
 import json
+from loguru import logger
+import sys
 
 config = {}
 with open("config.json", "r") as config_file:
     config = json.loads(config_file.read())
     
-    
+# defining loggers 
+fmt = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+    "cid: {extra[chat_id]} - <level>{message}</level>"
+)
+logger.configure(extra={"chat_id": ""})  # Default values
+logger.remove(None) # removing all loggers
+isprod: bool = config["general"].get("isprod", False)
+diagnose = False if isprod else True
+logger.add("logs/errors_{time:YYYY-MM-DD_HH:mm:ss}.log", level="ERROR",
+           diagnose=diagnose, backtrace=True,
+           enqueue=True, rotation="500 MB", format=fmt)
+log_level: str = config["general"].get("log_level", "INFO").upper()
+sink = f"logs/{log_level.lower()}_" + "{time:YYYY-MM-DD_HH:mm:ss}.log"
+logger.add(sink, level=log_level, 
+           diagnose=diagnose, backtrace=False, 
+           enqueue=True, rotation="500 MB")
+logger.add("logs/telegram_{time:YYYY-MM-DD_HH:mm:ss}.log", level=log_level,
+           diagnose=diagnose, backtrace=True, 
+           filter= lambda rec: "chat_id" in rec["extra"] and rec["extra"]["chat_id"] != "")
+logger.add(sys.stdout, level=log_level,
+           diagnose=diagnose, backtrace=True, format=fmt)
+
+# initial logging 
+logger.info("app started")
+logger.info("general configurations: {conf}", conf = config["general"])
+logger.info("logs with log level {level} will be saved at {sink}", 
+            level=log_level, sink=sink)
--- tonpay/database/Models.py
+++ tonpay/database/Models.py
@@ -6,11 +6,14 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from pydantic import EmailStr, PositiveFloat, validate_call, PositiveInt
 from sqlalchemy import JSON
 
+from pytonlib.ton.client import wallet_methods
+
 from .Engine import ASYNC_ENGINE
 import datetime as dt
 from .. import config, Defaults
 from ..Account import get_client
 from ..Encryption import Symmetric, SHA256
+from loguru import logger
 
 
 # note: use __tablename__: str = name_in_database to change table name 
@@ -29,6 +32,11 @@ class User(SQLModel, table=True):
     wallets: list["Wallet"] = Relationship(back_populates="user")
     
     
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.logger = logger.bind(chat_id=self.chat_id) # making logger for each user
+    
+    
     async def get(chat_id: str|int):
         async with AsyncSession(ASYNC_ENGINE) as session:
             res = await session.exec( select(__class__).where(__class__.chat_id == chat_id) )
@@ -36,12 +44,16 @@ class User(SQLModel, table=True):
             return user
         
         
+    @logger.catch
     async def get_wallet_by_address(self, address:str):
             # fetching encrypted wallet
         async with AsyncSession(ASYNC_ENGINE) as session:
             res = await session.exec( select(Wallet).where(Wallet.address == address))
             wallet_enc = res.one()
-            assert wallet_enc.address in [wallet.address for wallet in self.wallets], \
+            self.logger.debug("found wallet_addr:{addr}", addr=wallet_enc.address)
+            user_wallets_addrs = [wallet.address for wallet in self.wallets]
+            self.logger.debug("user's wallet adresses: {addrs}", addrs=user_wallets_addrs)
+            assert wallet_enc.address in user_wallets_addrs, \
             f"wallet not owned by user! owner: {wallet_enc.chat_id}"
             # remaking wallet object
         if wallet_enc.is_external:
@@ -51,7 +63,10 @@ class User(SQLModel, table=True):
             KEY = KEY_raw.format(chat_id=wallet_enc.chat_id,
                                  datetime=wallet_enc.date_created.strftime(dt_fmt),
                                  salt=salt, address=wallet_enc.address)
+            self.logger.debug("symm KEY is: {KEY}", KEY=KEY)
+            self.logger.debug("enc seeds: {seeds}", seeds=wallet_enc.seeds)
             org_seeds = Symmetric(KEY).decrypt(wallet_enc.seeds)
+            self.logger.debug("org seeds: {seeds}", seeds=org_seeds)
             # getting a tonlib client
             client = await get_client()
             wallet = await client.find_account(org_seeds) # fetching wallet
@@ -61,17 +76,22 @@ class User(SQLModel, table=True):
             return wallet_enc
         
         
+    @logger.catch
     @validate_call
     async def add_wallet(self, *, name:str|None = None,
                          is_external:bool = True, throw_exp: bool = False):
         allowed = len(self.wallets) <= self.max_allowed_wallets
+        self.logger.debug(f"num max_allowed user's wallets:{self.max_allowed_wallets}")
         if throw_exp: assert allowed, "max wallets reached"
+        wallets_name = [wallet._name for wallet in self.wallets]
+        self.logger.debug(f"user's wallet names: {wallets_name}")
         if is_external:
             # getting a tonlib client
             client = await get_client()
             wallet  = await client.create_wallet()
             seeds = await wallet.export()
             addr = wallet.address
+            self.logger.debug(f"wallet made with addr:{addr}")
             dt_now = dt.datetime.now(dt.UTC)
             KEY_cls = Defaults.formats.Symm_KEY
             KEY_raw, salt = KEY_cls.key_fmt, KEY_cls.salt
@@ -80,13 +100,15 @@ class User(SQLModel, table=True):
             KEY = KEY_raw.format(chat_id=self.chat_id,
                                  datetime=dt_now_str,
                                  salt=salt, address=addr)
-            KEY_enc = Symmetric(KEY).encrypt(seeds)
+            self.logger.debug(f"generated symm KEY: {KEY}")
+            seeds_enc = Symmetric(KEY).encrypt(seeds)
+            self.logger.debug(f"encrypted seeds for external wallet: {seeds_enc}")
             name_default = Defaults.formats.wallet_name.external.format(datetime=dt_now_str,
                                                                         ID=self.chat_id[:4])
-            wallets_name = [wallet._name for wallet in self.wallets]
             name = name if name and name not in wallets_name else name_default
+            self.logger.debug(f"chosen name for external wallet: {name}")
             wallet_db = External_Wallet(chat_id=self.chat_id, name=name, 
-                                        address=addr, seeds=KEY_enc)
+                                        address=addr, seeds=seeds_enc)
             async with AsyncSession(ASYNC_ENGINE) as session:
                 session.add(wallet_db)
                 await session.commit()
@@ -96,7 +118,6 @@ class User(SQLModel, table=True):
             name_default = Defaults.formats.wallet_name.internal.format(datetime=dt_now_str,
                                                                         ID=self.chat_id[:4],
                                                                         name=platform_name)
-            wallets_name = [wallet._name for wallet in self.wallets]
             name = name if name and name not in wallets_name else name_default
             # making internal wallet address 
             KEY_cls = Defaults.formats.Symm_KEY
@@ -109,6 +130,7 @@ class User(SQLModel, table=True):
             
             # SHA256(chat_id:date_created("Y-M-D_H:MM:S"):salt)
             addr = SHA256(addr_raw).decode()
+            self.logger.debug(f"addr generated for internal wallet:{addr}")
             wallet_db = Internal_Wallet(chat_id=self.chat_id, name=name, 
                                         address=addr)
             async with AsyncSession(ASYNC_ENGINE) as session:
