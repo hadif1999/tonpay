45ecdebcd696b3b16a06495c620a1a92df6688f1
Your Name <you@example.com>
2024-12-01 11:33:09 +0000

    install.sh added

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
-sudo apt-get update -y
-sudo apt-get install -y python3
+sudo apt-get update -y && sudo apt-get install -y python3 python3-dev build-essential libssl-dev libffi-dev python3-venv python3-pip libpd-dev
 python3 -m venv venv 
 source ./venv/bin/activate
-sudo apt-get install -y build-essential libssl-dev libffi-dev
-sudo apt-get -y install libpq-dev python3-dev
 pip3 install -U setuptools wheel pip
 sudo apt-get install -y python3-psycopg2
 pip3 install -r requirements.txt
